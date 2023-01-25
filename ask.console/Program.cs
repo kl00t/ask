999d64b3de2dd0c03e43e4e2d53fffe7599f5a8a
Scott Vaughan <scott.vaughan@ensono.com>
2023-01-25 10:05:39 +0000

    Add project files.

---
+++ ask.console/Program.cs
@@ -0,0 +1,11 @@
+﻿if (args.Length > 0)
+{
+    var apiKey = "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";
+    HttpClient httpClient = new ();
+    httpClient.DefaultRequestHeaders.Add("authorization", $"Bearer {apiKey}");
+
+}
+else
+{
+    Console.WriteLine("You need to provide some input.");
+}
